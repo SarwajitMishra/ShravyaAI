712fa73c6ce098f0803351e8c0e42436ffb94d8c
Sarwajit Mishra <sarwajit.mishra.cvrce@gmail.com>
2025-09-14 09:25:47 +0000

    6000-firebase-studio-1755131474336.cluster-nzwlpk54dvagsxetkvxzbvslyi.cl

--- next.config.js
+++ next.config.js
@@ -1,3 +1,4 @@
+
 // @ts-check
 
 const securityHeaders = [
@@ -8,7 +9,7 @@ const securityHeaders = [
       "script-src 'self' 'unsafe-inline' 'unsafe-eval' https:",
       "style-src 'self' 'unsafe-inline' https:",
       "img-src 'self' data: https:",
-      "connect-src 'self' https: wss://ws-proxy-709848175384.us-central1.run.app wss://livevoicepipeline-m7rijrszka-uc.a.run.app",
+      "connect-src 'self' https: wss://ws-proxy-709848175384.us-central1.run.app wss://livevoicepipeline-m7rijrszka-uc.a.run.app ws://localhost:8080",
       "frame-ancestors 'self'",
       "object-src 'none'",
       "base-uri 'self'",
